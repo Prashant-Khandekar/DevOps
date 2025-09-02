71e644ff1ed6887e04345fb8c6a4f80a8316e8bb
Prashant-Khandekar <prashantkhandekar@gmail.com>
2025-09-02 13:42:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function performOperation(operation) {
+    
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    if (isNaN(num1) || isNaN(num2)) {
+        document.getElementById("result").innerText = "Please enter valid numbers.";
+        return;
+    }
+
+    let result;
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            if (num2 === 0) {
+                document.getElementById("result").innerText = "Cannot divide by zero!";
+                return;
+            }
+            result = num1 / num2;
+            break;
+        default:
+            document.getElementById("result").innerText = "Invalid operation.";
+            return;
+    }
+    document.getElementById("result").innerText = `Result: ${result}`;
+}
